fb1cba3bda8deeefc8984b28ae492bcb1e7989b3
freewind <nowindlee@gmail.com>
2018-08-08 15:12:13 +0800

    finally recoding is ok

--- README.md
+++ README.md
@@ -1,18 +1,28 @@
-Android Hello World Project
-===========================
+Android Record Voice Project
+============================
 
-需要在`app/build.gradle`中增加：
+使用Android来录音，坑很多。
 
-```
-sourceSets {
-    main.java.srcDirs += 'src/main/kotlin/'
-    test.java.srcDirs += 'src/test/kotlin/'
-    androidTest.java.srcDirs += 'src/androidTest/Kotlin'
-}
-```
+初始化的坑
+-----
 
-这样才能识别`kotlin`目录。否则，`.kt`文件只能放在`java`目录下。
+不论是页面上的component，还是文件，还是Recorder/Player，都必须在`onCreate`方法调用以后才能初始化，否则异常。所以要把它们放到`lazy`里（还好kotlin突出提供了lazy）
 
-```
-./gradlew installRunDebug
-```
+权限的坑
+-------
+
+- `AndroidManifest.xml`里面要加权限：`<uses-permission android:name="android.permission.RECORD_AUDIO"/>`
+- 由于录音属于“危险权限”，所以光加上面的还不够，必须在运行时先向用户请求权限：`ActivityCompat.requestPermissions`，用户同意以后，才可以调用录音相关的API
+- 可使用`ActivityCompat.checkSelfPermission`来检查用户是否同意过某个权限
+- 在`ActivityCompat.requestPermissions`时传入的第三个参数`requestCode`是由自己定义的，可用来在`onRequestPermissionsResult`检查结果
+
+录音的坑
+-------
+
+- 不论是录音`MediaPlayer`还是播放`MediaPlayer`，这两个类都有自己内部维护的状态机。所以`reset/start/stop/release`这几个方法一定要在理解后小心调用。参看：<https://developer.android.com/reference/android/media/MediaRecorder>
+- 如果开始录音但是没有录到声音，调用`recorder.stop()`方法居然会抛异常！所以要针对这个情况处理（就是catch一下这个exception）
+
+Resource
+--------
+
+- States: <https://developer.android.com/reference/android/media/MediaRecorder>
--- app/src/main/java/demos/MainActivity.kt
+++ app/src/main/java/demos/MainActivity.kt
@@ -1,13 +1,100 @@
 package demos
 
-import android.support.v7.app.AppCompatActivity
+import android.Manifest
+import android.content.pm.PackageManager
+import android.media.MediaPlayer
+import android.media.MediaRecorder
 import android.os.Bundle
+import android.support.v4.app.ActivityCompat
+import android.support.v7.app.AppCompatActivity
+import android.view.MotionEvent
+import android.widget.Button
+import java.io.File
 
 class MainActivity : AppCompatActivity() {
+
+    private val MY_RECORD_AUDIO_REQUEST_CODE = 111
+
+    private val button by lazy<Button> { findViewById(R.id.button) }
+
+    private val audioFile by lazy { File(filesDir, "voice.mp4") }
+
+    private val recorder by lazy { MediaRecorder() }
+
+    private val player by lazy {
+        MediaPlayer()
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setupButton(button)
+    }
+
+    private fun setupButton(button: Button) {
+        button.setOnTouchListener { _, event ->
+            when (event.action) {
+                MotionEvent.ACTION_DOWN -> {
+                    if (hasRecordPermission()) {
+                        button.text = "Recording ..............."
+                        setupRecorder(recorder)
+                        recorder.setOutputFile(audioFile.absolutePath)
+                        recorder.prepare()
+                        recorder.start()
+                    } else {
+                        ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.RECORD_AUDIO), MY_RECORD_AUDIO_REQUEST_CODE)
+                    }
+                }
+                MotionEvent.ACTION_UP -> {
+                    button.text = "Record Voice"
+                    if (hasRecordPermission()) {
+                        if (stopRecording()) {
+                            play()
+                        }
+                    }
+                }
+            }
+            true
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        when (requestCode) {
+            MY_RECORD_AUDIO_REQUEST_CODE -> {
+                println("Permission request for recording voice: $requestCode")
+                println("permissions: $permissions")
+                println("results: $grantResults")
+            }
+            else -> println("Not my requests: $requestCode")
+        }
+    }
+
+    private fun stopRecording(): Boolean {
+        return try {
+            recorder.stop()
+            true
+        } catch (e: Exception) {
+            println(e.toString())
+            false
+        }
+    }
+
+    private fun hasRecordPermission() = ActivityCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) == PackageManager.PERMISSION_GRANTED
+
+    private fun setupRecorder(recorder: MediaRecorder) {
+        recorder.reset()
+        recorder.setAudioSource(MediaRecorder.AudioSource.MIC)
+        recorder.setOutputFormat(MediaRecorder.OutputFormat.MPEG_4)
+        recorder.setAudioEncoder(MediaRecorder.AudioEncoder.AAC)
+        recorder.setAudioEncodingBitRate(128000)
+    }
+
+    private fun play() {
+        player.reset()
+        player.setDataSource(audioFile.absolutePath)
+        player.prepare()
+        player.start()
     }
 
 }
